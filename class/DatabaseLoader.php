2ab53ef24b970dda78b119e0b981f9f3ad636755
naf <naf@auri.ga>
2021-02-15 14:34:08 +0900

    dotenv追加

--- class/DatabaseLoader.php
+++ class/DatabaseLoader.php
@@ -5,6 +5,8 @@ namespace Hostdon;
 
 
 use Illuminate\Database\Capsule\Manager as Capsule;
+use Dotenv\Dotenv;
+
 
 class DatabaseLoader
 {
@@ -15,14 +17,16 @@ class DatabaseLoader
 
     function __construct()
     {
+        $dotenv = Dotenv::createImmutable(__DIR__.'../');
+        $dotenv->load();
         $capsule = new Capsule;
 
         $capsule->addConnection([
             'driver'    => 'mysql',
-            'host'      => DB_HOST,
-            'database'  => DB_NAME,
-            'username'  => USER,
-            'password'  => PASSWORD,
+            'host'      => $_ENV['DB_HOST'],
+            'database'  => $_ENV['DB_NAME'],
+            'username'  => $_ENV['DB_USER'],
+            'password'  => $_ENV['DB_PASS'],
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
