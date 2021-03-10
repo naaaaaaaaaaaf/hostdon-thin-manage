b2b2dfd18f0d966e2b9ec3bd838109b95aec01f3
naf <naf@auri.ga>
2021-03-10 10:22:30 +0900

    バリデーション時のメッセージ配列を再構築して返却するメソッドを追加

--- class/MessageConstructs.php
+++ class/MessageConstructs.php
@@ -7,12 +7,25 @@ namespace Hostdon;
 class MessageConstructs
 {
     private array $messages;
-    function __construct(){
+
+    function __construct()
+    {
         $this->messages = array();
     }
-    public function add_message(string $msg, string $level): string
+
+    public function add_message(string $text, string $level): string
     {
-    $data = "<div class=\"alert alert-${level}\" role=\"alert\">${msg}</div>";
-    return $this->messages[] = $data;
+        $data = "<div class=\"alert alert-${level}\" role=\"alert\">${text}</div>";
+        return $this->messages[] = $data;
+    }
+    public function reconstruct_message(array $messages_array,string $level): array
+    {
+        $data = array();
+        foreach ($messages_array as $values){
+            foreach ($values as $value ){
+                $data[] = $this->add_message($value,$level);
+            }
+        }
+        return $data;
     }
 }
